0a3ad80d937cb1d3ebd52d4f4ff75381402367d2
Francesco Noacco <francesco.noacco@secomind.com>
2023-09-04 15:35:52 +0200

    fix(repo): now compiles
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- lib/astarte_data_access/repo.ex
+++ lib/astarte_data_access/repo.ex
@@ -1,3 +1,12 @@
 defmodule Astarte.DataAccess.Repo do
   use Ecto.Repo, otp_app: :astarte_data_access, adapter: Exandra
+  alias Astarte.DataAccess.Config
+
+  def init(_context, config) do
+    config =
+      Config.xandra_options!()
+      |> Keyword.merge(config)
+
+    {:ok, config}
+  end
 end
